c496308bbcdc6cb45817139a45afc6c0028fd046
zacharygilliom <zacharygilliom@gmail.com>
2020-02-29 15:05:18 -0500

    Added Notes and took out unneeded print statements.

--- main.py
+++ main.py
@@ -17,49 +17,53 @@ def summary(df):
 		print(str(i) + ": " + str(len(list(df[i].unique()))))
 		print('NA Values: ' + str(df[i].isna().sum()))
 
-def set_params(ax, title, xtitle, ytitle):
+def set_params(ax, title, xtitle, ytitle, facecolor):
 	ax.set_title(title)
 	ax.set_xlabel(xtitle)
 	ax.set_ylabel(ytitle)
+	ax.set_facecolor(facecolor)
 
-
+# Slicing dataframe to only look at active bookings.
 df_not_canceled = df[df['is_canceled'] == 0]
 
+# Quick summary of what our data looks like
 summary(df_not_canceled)
 
-df['adults'] = df['adults'].astype('int32')
-
-print(df_not_canceled['lead_time'].head(15), df_not_canceled['adults'].head(15))
-
+# Creating subplots and styling figure.
 fig, ax=plt.subplots(nrows=4)
+fig.set_facecolor("lightslategray")
+
+# Building our Four Plots.
 sns.barplot(x='arrival_date_month', y='stays_in_week_nights', data=df_not_canceled, hue='arrival_date_year', ci=None, ax=ax[0], edgecolor='black')
 sns.kdeplot(data=df_not_canceled['adults'], shade=False, ax=ax[1])
 sns.kdeplot(data=df_not_canceled['children'], shade=False, ax=ax[1])
 sns.barplot(x='lead_time', y='adults', data=df_not_canceled, ax=ax[2], ci=None, orient='h', edgecolor='black')
 sns.barplot(x='lead_time', y='children', data=df_not_canceled, ax=ax[3], ci=None, orient='h', edgecolor='black')
-fig.set_facecolor("lightslategray")
-# ax[0].edgecolor = .15
-ax[0].set_facecolor("lightslategray")
-ax[1].set_facecolor("lightslategray")
-ax[2].set_facecolor("lightslategray")
-ax[3].set_facecolor("lightslategray")
 
+
+# Setting labels for our plots.
 set_params(ax[0], 
 		title='Month of Hotel Arrival vs # of Weekdays Stayed',
 		xtitle='Month of Arrival',
-		ytitle='# of Weekdays')
+		ytitle='# of Weekdays',
+		facecolor="lightslategray")
 set_params(ax[1],
 		title='Distribution of Number of Children & Adults',
 		xtitle='Number',
-		ytitle='Kernel Distribution')
+		ytitle='Kernel Distribution',
+		facecolor="lightslategray")
 set_params(ax[2],
 		title='Number of Adults vs Lead Time of Booking',
 		xtitle='Lead Time in Days',
-		ytitle='Number of Adults')
+		ytitle='Number of Adults',
+		facecolor="lightslategray")
 set_params(ax[3],
 		title='Number of Children vs Lead Time of Booking',
 		xtitle='Lead Time in Days',
-		ytitle='Number of Children')
+		ytitle='Number of Children',
+		facecolor="lightslategray")
+
+# Tight Layout prevents our labels from overlapping.
 plt.tight_layout(pad=.15)
 plt.show()
 
