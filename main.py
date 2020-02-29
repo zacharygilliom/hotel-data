3df329a16714685cfd1c124fc36d1acf2215752d
zacharygilliom <zacharygilliom@gmail.com>
2020-02-29 00:24:13 -0500

    did more styling of visuals.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import seaborn as sns
 
 # Set the background style we want
 sns.set_style('darkgrid')
-sns.set_palette('Blues_d')
+sns.set_palette(sns.cubehelix_palette())
 
 df = pd.read_csv('hotel_bookings.csv')
 
@@ -15,12 +15,29 @@ def summary(df):
 	print('Unique Values in Each Column')
 	for i in df.columns:
 		print(str(i) + ": " + str(len(list(df[i].unique()))))
+		print('NA Values: ' + str(df[i].isna().sum()))
 
 df_not_canceled = df[df['is_canceled'] == 0]
 
 summary(df_not_canceled)
 
-fig, ax=plt.subplots(nrows=2)
+df['adults'] = df['adults'].astype('int32')
+
+print(df_not_canceled['lead_time'].head(15), df_not_canceled['adults'].head(15))
+
+fig, ax=plt.subplots(nrows=4)
 sns.barplot(x='arrival_date_month', y='stays_in_week_nights', data=df_not_canceled, hue='arrival_date_year', ci=None, ax=ax[0])
 sns.kdeplot(data=df_not_canceled['adults'], shade=False, ax=ax[1])
+sns.kdeplot(data=df_not_canceled['children'], shade=False, ax=ax[1])
+sns.barplot(x='lead_time', y='adults', data=df_not_canceled, ax=ax[2], ci=None, orient='h')
+sns.barplot(x='lead_time', y='children', data=df_not_canceled, ax=ax[3], ci=None, orient='h')
+fig.set_facecolor("slategray")
+ax[0].set_facecolor("lightslategray")
+ax[1].set_facecolor("lightslategray")
+ax[2].set_facecolor("lightslategray")
+ax[3].set_facecolor("lightslategray")
+# ax[1].set_axis_bgcolor("lightslategray")
+# ax[2].set_axis_bgcolor("lightslategray")
+# ax[3].set_axis_bgcolor("lightslategray")
 plt.show()
+
