a0d720af043ef5c6b40c9abb0182736a78e3d28b
zacharygilliom <zacharygilliom@gmail.com>
2020-03-01 12:31:03 -0500

    Added third figure, and fixed some styling.

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ from matplotlib import interactive
 
 # Set the background style we want
 sns.set_style('darkgrid', {"axes.edgecolor": ".15", "grid.color": ".15"})
-sns.set_palette('Set2')
+sns.set_palette('Blues_d')
 
 df = pd.read_csv('hotel_bookings.csv')
 
-print(df[df['babies'] > 3].head())
-
 def summary(df):
 	print(df.head())
-	print(df.columns)
 	print(df.dtypes)
 	print('Unique Values in Each Column')
 	for i in df.columns:
@@ -30,7 +27,7 @@ def set_params(ax, title, xtitle, ytitle, facecolor):
 # There are also two outliers where the number of babies is higher and it skews the data and makes the visuals 
 # less visually appealing.
 df_not_canceled = df[(df['is_canceled'] == 0) & (df['babies'] < 4)]
-
+df_not_canceled['children'] = df_not_canceled['children'].astype('int64')
 # Quick summary of what our data looks like
 summary(df_not_canceled)
 
@@ -70,22 +67,40 @@ set_params(ax[3],
 		ytitle='Number of Children',
 		facecolor="lightslategray")
 
-ax[0].legend(ncol=2, loc='upper right',framealpha=.5)
+ax[0].legend(ncol=2, loc='upper right', framealpha=.5)
 
 # Tight Layout prevents our labels from overlapping.
 plt.tight_layout(pad=.15)
 fig.show()
 
 # Creating our Second Figure and styling it.
-fig2, ax = plt.subplots(nrows=1)
-fig2 = plt.figure(2)
+fig2, ax = plt.subplots(nrows=3, num='Analysis by Day of Month')
 fig2.set_facecolor('lightslategray')
 
 # Creating our subplots.
-sns.barplot(x='adults', y='lead_time', data=df_not_canceled, ci=None, ax=ax, edgecolor='black')
-ax.set_facecolor('lightslategray')
+sns.barplot(x='arrival_date_day_of_month', y='lead_time', hue='hotel', data=df_not_canceled, ci=None, ax=ax[0], edgecolor='black')
+ax[0].set_facecolor('lightslategray')
+ax[0].legend(ncol=2, loc='upper right', framealpha=.5)
+
+sns.barplot(x='arrival_date_day_of_month', y='lead_time', hue='meal', data=df_not_canceled, ci=None, ax=ax[1], edgecolor='black')
+ax[1].set_facecolor('lightslategray')
+ax[1].legend(ncol=2, loc='upper right', framealpha=.5)
+
+sns.kdeplot(data=df_not_canceled['lead_time'], shade=True, ax=ax[2])
+ax[2].set_facecolor('lightslategray')
+ax[2].legend(framealpha=.5)
+plt.tight_layout(pad=.15)
 
 fig2.show()
+
+# Creating our 3rd Subplot
+fig3, ax = plt.subplots(nrows=1, num='Violin Plots')
+fig3.set_facecolor('lightslategray')
+
+sns.violinplot(x='meal', y='lead_time', hue='hotel', data=df_not_canceled, ax=ax)
+ax.set_facecolor('lightslategray')
+
+fig3.show()
 
 # Since we are creating two figures, in order to show them simultaneously we need to ask for an input to
 # keep our program running.
