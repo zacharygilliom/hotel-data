db723cc9f305a2c2a7f2a0025bc92e4fc4bb4cc7
zacharygilliom <zacharygilliom@gmail.com>
2020-03-07 22:45:09 -0500

    added 4th figure showing the most expensive countries.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sns
+import numpy as np
 from matplotlib import interactive
 
 # Set the background style we want
@@ -38,32 +39,37 @@ breakfast = {'SC': 'No meal', 'BB': 'Bed & Breakfast', 'HB': 'Half Board', 'FB':
 # Slicing dataframe to only look at active bookings.
 # There are also two outliers where the number of babies is higher and it skews the data and makes the visuals 
 # less visually appealing.
-df_not_canceled = df[(df['is_canceled'] == 0)
-	& (df['babies'] < 4)
+df_filter = df[(df['babies'] < 4)
 	& (df['adr'] < 1000)
 	& (df['adr'] > 0)
 	]
 
-df_not_canceled['children'] = df_not_canceled['children'].astype('int64')
 
-df_not_canceled = change_meals(df=df_not_canceled, di=breakfast)
+# df_filter['children'] = df_filter['children'].astype('int64')
+
+df_filter = change_meals(df=df_filter, di=breakfast)
+
+
+df_country_filter = df_filter.groupby(['country', 'hotel']).mean().nlargest(20, 'adr').reset_index()
+
+print(df_country_filter)
 
 # Quick summary of what our data looks like
-summary(df_not_canceled)
+# summary(df_filter)
 
 # Creating subplots and styling of our first figure.
 fig, ax=plt.subplots(nrows=4, num='Graphical Analysis of Lead Time vs Types of People')
 fig.set_facecolor("lightslategray")
 
 # Building our four plots for figure 1.
-sns.kdeplot(data=df_not_canceled['adults'], shade=False, ax=ax[0])
-sns.kdeplot(data=df_not_canceled['children'], shade=False, ax=ax[0])
-sns.kdeplot(data=df_not_canceled['babies'], shade=False, ax=ax[0])
+sns.kdeplot(data=df_filter['adults'], shade=False, ax=ax[0])
+sns.kdeplot(data=df_filter['children'], shade=False, ax=ax[0])
+sns.kdeplot(data=df_filter['babies'], shade=False, ax=ax[0])
 ax[0].legend(ncol=2, loc='upper right', framealpha=.5)
 
-sns.barplot(x='lead_time', y='babies', data=df_not_canceled, ax=ax[1], ci=None, orient='h', edgecolor='black')
-sns.barplot(x='lead_time', y='adults', data=df_not_canceled, ax=ax[2], ci=None, orient='h', edgecolor='black')
-sns.barplot(x='lead_time', y='children', data=df_not_canceled, ax=ax[3], ci=None, orient='h', edgecolor='black')
+sns.barplot(x='lead_time', y='babies', data=df_filter, ax=ax[1], ci=None, orient='h', edgecolor='black')
+sns.barplot(x='lead_time', y='adults', data=df_filter, ax=ax[2], ci=None, orient='h', edgecolor='black')
+sns.barplot(x='lead_time', y='children', data=df_filter, ax=ax[3], ci=None, orient='h', edgecolor='black')
 
 
 # Setting labels for our first plots.
@@ -99,16 +105,16 @@ fig2.set_facecolor('lightslategray')
 
 # Creating our 3 subplots and styling them.
 sns.barplot(x='arrival_date_month', y='adr', hue='hotel',
-	data=df_not_canceled, ci=None, ax=ax[0], edgecolor='black', order=Months)
+	data=df_filter, ci=None, ax=ax[0], edgecolor='black', order=Months)
 
 ax[0].legend(ncol=2, loc='upper right', framealpha=.5)
 
 sns.barplot(x='arrival_date_month', y='adr', hue='meal',
-	data=df_not_canceled, ci=None, ax=ax[1], edgecolor='black', order=Months)
+	data=df_filter, ci=None, ax=ax[1], edgecolor='black', order=Months)
 
 ax[1].legend(ncol=2, loc='upper right', framealpha=.5)
 
-sns.kdeplot(data=df_not_canceled['adr'], shade=True, ax=ax[2])
+sns.kdeplot(data=df_filter['adr'], shade=True, ax=ax[2])
 ax[2].legend(framealpha=.5)
 
 # Set labels for our charts.
@@ -138,8 +144,7 @@ fig3, ax = plt.subplots(nrows=1,
 	num='Distribution of Meal Types at Each Type of Hotel and Avg. Daily Rate')
 fig3.set_facecolor('lightslategray')
 
-sns.violinplot(x='meal', y='adr', hue='hotel', data=df_not_canceled, ax=ax)
-ax.set_facecolor('lightslategray')
+sns.violinplot(x='meal', y='adr', hue='hotel', data=df_filter, ax=ax)
 
 # Set labels for our violin plot.
 set_params(ax,
@@ -149,6 +154,22 @@ set_params(ax,
 		facecolor='lightslategray')
 
 fig3.show()
+
+# Fourth Figure
+pal=sns.color_palette('Blues_d', len(df_country_filter))
+fig4, ax=plt.subplots(nrows=1,
+	num='Top 20 Most Expensive Countries by Average Daily Rate')
+fig4.set_facecolor('lightslategray')
+
+sns.barplot(x='adr', y='country', data=df_country_filter, ax=ax, ci=None, orient='h', palette=np.array(pal[:]))
+set_params(ax,
+		title='Top 20 Highest Average Daily Rate',
+		xtitle='Price',
+		ytitle='Country',
+		facecolor='lightslategray')
+
+fig4.show()
+
 
 # Since we are creating two figures, in order to show them simultaneously we need to ask for an input to
 # keep our program running.
